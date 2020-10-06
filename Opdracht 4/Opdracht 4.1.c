8e123673a312eaa31875ee32ef23733d82569675
Teun <teun_h@live.nl>
2020-10-06 13:18:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- Opdracht 4/Opdracht 4.1.c
+++ Opdracht 4/Opdracht 4.1.c
@@ -8,9 +8,19 @@
 int main()                                    // Main function
 {
   {
+<<<<<<< Updated upstream
     drive_goto(256, 256);
     pause(200);
     drive_goto(26, -25);                      // Stop
+=======
+    drive_speed(30.76923077, 30.76923077);    // Drive speed forward = 30,8 tps left and right
+    pause(5000);                              // Drive time = 5 s
+    drive_speed(0.0, 0.0);                    // No drive speed
+    pause(3000);                              // Stand still for 3 s
+    drive_speed(-30.76923077, -30.76923077);  // Drive speed backward = 30,8 tps left and right
+    pause(5000);                              // Drive time = 5 s
+    drive_speed(0, 0);                        // Stop
+>>>>>>> Stashed changes
   }
 }      
 
